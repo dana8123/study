593dceb9479ef19ae114f1a38d2106b1671ed6ee
danaisboss <yjk9313@gmail.com>
2021-07-18 00:41:44 +0900

    scope

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,27 +1,69 @@
-'use strict';
+"use strict";
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
-  function printAge() {
-    const output = `${firstname}, You are ${age}, born in ${birthYear}`;
-    console.log(output);
+/*function calcAge(birthYear) {
+	const age = 2037 - birthYear;
+	function printAge() {
+		let output = `${firstname}, You are ${age}, born in ${birthYear}`;
+		console.log(output);
 
-    if (birthYear >= 1991 && birthYear <= 1996) {
-      var millenial = true;
-      const firstname = 'steven';
-      const str = `Oh you're a millenial! ${firstname}`;
-      console.log(str);
+		if (birthYear >= 1991 && birthYear <= 1996) {
+			var millenial = true;
+			const firstname = "steven";
+			const str = `Oh you're a millenial! ${firstname}`;
+			console.log(str);
 
-      function add(a, b) {
-        return a + b;
-      }
-    }
-    console.log(millenial); //function scope
-    //console.log(add(2, 3));
-  }
-  printAge();
-  return age;
+			function add(a, b) {
+				return a + b;
+			}
+
+			output = "New output";
+		}
+		console.log(millenial); //function scope
+		//console.log(add(2, 3));
+		console.log(output);
+	}
+	printAge();
+	return age;
 }
 
-const firstname = 'jonas';
+const firstname = "jonas";
 calcAge(1994);
+
+
+//variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = "Jonas";
+let job = "student";
+const year = 1991;
+
+//function
+console.log(addDecl(2, 3));
+console.log(addExpr(2, 3));
+console.log(arrow(2, 3));
+
+function addDecl(a, b) {
+	return a + b;
+}
+const addExpr = function (a, b) {
+	return a + b;
+};
+const arrow = (a, b) => a + b;
+*/
+
+if (!numProduct) deleteShoppingCart();
+
+var numProduct = 10;
+
+function deleteShoppingCart() {
+	console.log("All product deleted!");
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
